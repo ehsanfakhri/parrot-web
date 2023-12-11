093b9615783e1616362c92ba90aa0d896a48cee9
ehsanfakhri <fakhri.ehsan@gmail.com>
2023-12-11 23:20:09 +0330

    Add WorkPage + ServicePage + TestimonialsPage + ContatcPage

--- app/components/Bulb.jsx
+++ app/components/Bulb.jsx
@@ -1,3 +1,16 @@
+// next image
+import Image from "next/image";
+
 export default function Bulb() {
-  return <div>Bulb</div>;
+  return (
+    <div className="absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75 z-10 w-[200px] xl:w-[260px]">
+      <Image
+        src={"/bulb.png"}
+        width={260}
+        height={200}
+        alt=""
+        className="w-full h-full"
+      />
+    </div>
+  );
 }
