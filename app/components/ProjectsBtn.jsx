d9262931e352e9580fe2171953c741be2339e4a4
ehsanfakhri <fakhri.ehsan@gmail.com>
2023-12-17 02:50:03 +0330

    Design Update....

--- app/components/ProjectsBtn.jsx
+++ app/components/ProjectsBtn.jsx
@@ -9,7 +9,7 @@ import { HiArrowRight } from "react-icons/hi2";
 
 export default function ProjectsBtn() {
   return (
-    <div className="mx-auto xl:mx-0">
+    <div className="mx-auto xl:mx-0 z-20">
       <Link
         href={"/work"}
         className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
@@ -19,7 +19,7 @@ export default function ProjectsBtn() {
           width={141}
           height={148}
           alt=""
-          className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
+          className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px] z-10"
         />
         <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
       </Link>
