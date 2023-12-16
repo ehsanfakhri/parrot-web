088484dc4f8cc4970b4f43955e212fa14985c9d4
ehsanfakhri <fakhri.ehsan@gmail.com>
2023-12-17 01:37:57 +0330

    Add slidersUrl

--- app/components/WorkSlider.jsx
+++ app/components/WorkSlider.jsx
@@ -25,18 +25,22 @@ const workSlides = {
         {
           title: "title",
           path: "/thumb1.jpg",
+          slidesUrl: "https://web3lancer.vercel.app",
         },
         {
           title: "title",
           path: "/thumb2.jpg",
+          slidesUrl: "",
         },
         {
           title: "title",
           path: "/thumb3.jpg",
+          slidesUrl: "",
         },
         {
           title: "title",
           path: "/thumb4.jpg",
+          slidesUrl: "",
         },
       ],
     },
@@ -82,40 +86,45 @@ export default function WorkSlider() {
                     className="relative rounded-lg overflow-hidden flex
                    items-center justify-center group"
                   >
-                    <div
-                      className="relative flex items-center justify-center group"
-                    >
-                      {/* image */}
-                      <Image src={image.path} width={500} height={300} alt="" />
-                      {/* overlay gradient */}
-                      <div
-                        className="absolute inset-0 bg-gradient-to-l
+                    <a href={image.slidesUrl}>
+                      <div className="relative flex items-center justify-center group">
+                        {/* image */}
+                        <Image
+                          src={image.path}
+                          width={500}
+                          height={300}
+                          alt=""
+                        />
+                        {/* overlay gradient */}
+                        <div
+                          className="absolute inset-0 bg-gradient-to-l
                        from-transparent via-[#e838cc] to-[#4a22bd]
                         opacity-0 group-hover:opacity-80 transition-all duration-700 "
-                      ></div>
-                      {/* title */}
-                      <div
-                        className="absolute bottom-0 translate-y-full
+                        ></div>
+                        {/* title */}
+                        <div
+                          className="absolute bottom-0 translate-y-full
                        group-hover:-translate-y-10 group-hover:xl:-translate-y-20
                         transition-all duration-300 "
-                      >
-                        <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
-                          {/* title part 1 */}
-                          <div className="delay-100">LIVE</div>
-                          {/* title part 2 */}
-                          <div
-                            className="translate-y-[500%] group-hover:translate-y-0
+                        >
+                          <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                            {/* title part 1 */}
+                            <div className="delay-100">LIVE</div>
+                            {/* title part 2 */}
+                            <div
+                              className="translate-y-[500%] group-hover:translate-y-0
                            transition-all duration-300 delay-150"
-                          >
-                            PROJECT
-                          </div>
-                          {/* icon */}
-                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200 ">
-                            <BsArrowRight />
+                            >
+                              PROJECT
+                            </div>
+                            {/* icon */}
+                            <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200 ">
+                              <BsArrowRight />
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
+                    </a>
                   </div>
                 );
               })}
